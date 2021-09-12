e21a722274e56b0c91ca0dcd45ce341a443ae466
burntfalafel <wadhwae@tcd.ie>
2021-09-12 15:53:38 +0100

    [checkpoint] single linked list working fine. Added some signal handling - only killing child added. Now thinking of used single circular linked list. Also added a general util.* header

--- include/skud.c
+++ include/skud.c
@@ -21,37 +21,37 @@ typedef struct process_t
 {
   struct process_t *next;
   pid_t task_pid;
-  char *name;
   enum priority rank;
+  char name[20];
 } process_t;
 
 process_t* processes = NULL;
 int processlistlen;
 int running_tasks;
 
-static void new_process (process_t *processlist, char* processname, pid_t pid, enum priority pos)
+static void new_process (process_t **processlist, char* processname, pid_t pid, enum priority pos)
 {
   process_t* newprocess = malloc(sizeof(process_t));
   newprocess->task_pid = pid;
   newprocess->rank = pos;
   strcpy(newprocess->name, processname);
   processlistlen++;
-  processlist->next = NULL;
-  if (processlist==NULL)
+  newprocess->next = NULL;
+  if (*processlist==NULL)
   {
-    processlist = newprocess;
+    *processlist = newprocess;
     return;
   }
-  while(processlist!=NULL)
-    processlist=processlist->next;
-  processlist->next=newprocess;
+  while((*processlist)->next!=NULL)
+    *processlist=(*processlist)->next;
+  (*processlist)->next=newprocess;
   return;
 }
 
-static void remove_process (process_t *processlist, pid_t pid)
+static void remove_process (process_t **processlist, pid_t pid)
 {
-  process_t *itr = processlist;
-  while(itr->next!=NULL)
+  process_t *itr = *processlist;
+  while(itr!=NULL)
   {
     if(itr->task_pid == pid)
     {
@@ -61,18 +61,32 @@ static void remove_process (process_t *processlist, pid_t pid)
     }
     itr=itr->next;
   }
+  if(processlistlen==0)
+    *processlist = NULL;
   return;
+}
+
+process_t *find_process(process_t *processlist, pid_t pid)
+{
+  process_t *itr = processlist;
+  while(itr != NULL)
+  {
+    if(pid==itr->task_pid)
+      return itr;
+    itr = itr->next;
+  }
+  return NULL;
 }
 
 static void print_processes (process_t *processlist)
 {
-  while(processlist->next != NULL)
+  while(processlist != NULL)
   {
     printf("\nCurrently running %d of %d processes\n", running_tasks, processlistlen);
     if (processlist->rank == LOW)
-      printf("Process ID %d \t\t | \t\t Process Name %s \t\t | \t\t Priority: LOW \t\t |\n", processlist->task_pid, processlist->name);
+      printf("Process ID %d \t | \t Process Name: %s \t | \t Priority: LOW \t |\n", processlist->task_pid, processlist->name);
     else if (processlist->rank == HIGH)
-      printf("Process ID %d \t\t | \t\t Process Name %s \t\t | \t\t Priority: HIGH \t\t |\n", processlist->task_pid, processlist->name);
+      printf("Process ID %d \t | \t Process Name: %s \t | \tPriority: HIGH \t |\n", processlist->task_pid, processlist->name);
     processlist = processlist->next;
   }
   return;
@@ -102,14 +116,86 @@ static void skud_create_process(char *executable)
 {
   pid_t pid;
   pid = fork();
-  printf("ProcessID %d \n", pid);
+  if(pid == -1)
+  {
+    perror("unable to fork:skud_create_process");
+    exit(-1);
+  }
+  else if (pid==0)
+  {
+    /* initialize the required arguments for the execve() function call */
+		char *newargv[] = { executable, NULL };
+		char *newenviron[] = { NULL };
 
-  kill_by_id(processes, pid);
+		/* each process stops itself when created, and the 
+		 * scheduler starts operating when all the processes
+		 * are created and added to the process list */
+		raise(SIGSTOP); // equivalent to kill(getpid(), SIGSTOP);
+
+		execve(executable, newargv, newenviron);
+		/* execve() only returns on error */
+		perror("execve");
+		exit(1);
+  }
+  new_process(&processes, executable, pid, LOW);
   return;
 }
-int main()
+
+static int
+prioritize_process(pid_t pid, enum priority new_priority)
 {
-  skud_create_process("ls");
+  process_t *finditr = find_process(processes, pid);
+  if(!finditr)
+  {
+    fprintf(stderr, "Couldn't find pid %d hence failed new priority assignment", pid);
+    return 1;
+  }
+  finditr->rank = new_priority;
   return 0;
 }
-// https://github.com/dlekkas/proc_scheduler/blob/master/src/scheduler.c
+
+/* 
+ * SIGALRM handler
+ */
+static void
+sigalrm_handler(int signum)
+{
+	/* stop the execution of the current running process */
+	kill(processes->task_pid, SIGSTOP);
+}
+
+/* 
+ * SIGCHLD handler
+ */
+static void
+sigchld_handler(int signum)
+{
+	pid_t pid;
+	int status;
+	while(1)
+  {
+		/* wait for any child (non-blocking) 
+     * https://stackoverflow.com/questions/33508997/waitpid-wnohang-wuntraced-how-do-i-use-these/34845669 
+     */
+		pid = waitpid(-1, &status, WUNTRACED | WNOHANG);
+    if (pid<0)
+    {
+      perror("waitpid");
+      exit(pid);
+    } else if (pid==0)
+      break;
+#ifdef DEBUG
+    explain_wait_status(pid, status);
+#endif
+    
+
+int main()
+{
+  new_process(&processes, "Hello", 1234, LOW);
+  print_processes(processes);
+  prioritize_process(1234, HIGH);
+  print_processes(processes);
+  remove_process(&processes, 1234);
+  return 0;
+}
+
---
+++ include/util.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+
+/*
+ * This function receives an integer status value,
+ * as returned by wait()/waitpid() and explains what
+ * has happened to the child process.
+ *
+ * The child process may have:
+ *    * been terminated because it received an unhandled signal (WIFSIGNALED)
+ *    * terminated gracefully using exit() (WIFEXITED)
+ *    * stopped because it did not handle one of SIGTSTP, SIGSTOP, SIGTTIN, SIGTTOU
+ *      (WIFSTOPPED)
+ *
+ * For every case, a relevant diagnostic is output to standard error.
+ */
+void
+explain_wait_status(pid_t pid, int status)
+{
+	if (WIFEXITED(status))
+		fprintf(stderr, "My PID = %ld: Child PID = %ld terminated normally, exit status = %d\n",
+			(long)getpid(), (long)pid, WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		fprintf(stderr, "My PID = %ld: Child PID = %ld was terminated by a signal, signo = %d\n",
+			(long)getpid(), (long)pid, WTERMSIG(status));
+	else if (WIFSTOPPED(status))
+		fprintf(stderr, "My PID = %ld: Child PID = %ld has been stopped by a signal, signo = %d\n",
+			(long)getpid(), (long)pid, WSTOPSIG(status));
+	else {
+		fprintf(stderr, "%s: Internal error: Unhandled case, PID = %ld, status = %d\n",
+			__func__, (long)pid, status);
+		exit(1);
+	}
+	fflush(stderr);
+}
